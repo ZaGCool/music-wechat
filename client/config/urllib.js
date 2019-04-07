8e0e1940ac7d4f59d4faed3cb0a3be20bd990189
ZaG <1658261070@qq.com>
2019-04-07 12:50:32 +0800

    change files

---
+++ client/config/urllib.js
@@ -0,0 +1,6 @@
+let baseUrl = 'https://r3phkegj.qcloud.la'
+export default {
+  listUrl: baseUrl + '/top/list',
+  songUrl: baseUrl + '/song/detail',
+  lrcUrl: baseUrl + '/lyric'
+}
--- config/urllib.js
+++
@@ -1,6 +0,0 @@
-let baseUrl = 'http://localhost:3000'
-export default {
-  listUrl: baseUrl + '/top/list',
-  songUrl: baseUrl + '/song/detail',
-  lrcUrl: baseUrl + '/lyric'
-}
